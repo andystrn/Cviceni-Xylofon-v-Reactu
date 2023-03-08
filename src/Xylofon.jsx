db67eee6d0fcacbf6f2c99b9f4d578133264ec56
andystrn <an.strnadlova@gmail.com>
2023-03-08 15:33:59 +0100

    task done

--- src/Xylofon.jsx
+++ src/Xylofon.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { zahraj } from './zahraj'
 
 // 1. Nahraď jednotlivé tlačítka/kameny za výpis pole pomocí `.map`. Použij následující pole `kameny`.
-const kameny = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+const kameny = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 
 // 2. Přichystej si stavovou proměnnou `nadpis`, ve které jako výchozí hodnotu nastav „Xylofon“ a proměnnou vypiš uvnitř tagu `<h1>`.
 // 3. Po kliknutí na jakýkoliv kámen změň hodnotu nadpisu na text „Cink!“. Zkontroluj, že se nadpis opravdu změní.
@@ -11,19 +11,21 @@ const kameny = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 // 6. Zapni si v počítači zvuk a xylofon otestuj.
 
 export const Xylofon = () => {
+	const [nadpis, setNadpis] = useState('Xylofon');
+
+	const xylofonEvent = (e) => {
+		const ton = e.target.textContent;
+	
+		setNadpis(ton); 
+		zahraj(ton);
+	};	
+
 	return (
 		<>
-			<h1>Xylofon</h1>
+			<h1>{nadpis}</h1>
 			<div className="xylofon">
-				<button type="button" className="kamen">
-					C
-				</button>
-				<button type="button" className="kamen">
-					D
-				</button>
-				<button type="button" className="kamen">
-					E
-				</button>
+				{kameny.map((kamen) =>
+					<button onClick={(e) => xylofonEvent(e)} key={kamen} type="button" className="kamen">{kamen}</button>)}
 			</div>
 		</>
 	)
